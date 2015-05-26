b6dd9494baa5d97eb3b2dffb2e8cfbd00fec4ff8
isidurlll <isidurlll@gmail.com>
2015-05-26 21:20:02 +0200

    PruebasHabilidades

---
+++ Assets/Scripts/Habilidades/Enemigos/Spider/HandleSpiderArea.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleSpiderArea : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void HitSpiderAreaColision(GameObject pe_GOColision)
+    {
+        Debug.Log(pe_GOColision.name);
+    }
+}
