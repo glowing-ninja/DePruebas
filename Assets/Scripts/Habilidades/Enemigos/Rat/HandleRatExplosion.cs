625c5db61ca460b7835bf740cc5dabcce8d13265
isidurlll <isidurlll@gmail.com>
2015-05-27 19:45:25 +0200

    Mas pruebas

---
+++ Assets/Scripts/Habilidades/Enemigos/Rat/HandleRatExplosion.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleRatExplosion : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col.name);
+    }
+
+}
